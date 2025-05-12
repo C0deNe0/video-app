346376d6434be942656e044b294e7979550bec87
C0deNe0 <access.naveenhiremath@gmail.com>
2025-05-12 23:21:47 +0530

    completed backend and init frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Route,Routes,Switch} from "react-router-dom"
+
+import CreateRoom from "./components/CreateRoom"
+import Room from "./components/room"
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" Component={CreateRoom}/>
+              <Route path="/room/:roomID" Component={Room}/>
+          </Routes>
+          
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
--- server/signalling.go
+++ server/signalling.go
@@ -11,15 +11,15 @@ import (
 
 var AllRoom RoomMap
 
-type resp struct {
-	roomID string `json:"room_id"`
-}
-
 func CreateRoomRequestHandler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprint(w, "create room request Handler")
+	w.Header().Set("Access-control-Allow-Origin", "*")
 	roomID := AllRoom.CreateRoom()
 
-	json.NewEncoder(w).Encode(resp{roomID: roomID})
+	type resp struct {
+		RoomID string `json:"room_id"`
+	}
+	json.NewEncoder(w).Encode(resp{RoomID: roomID})
 
 }
 
@@ -27,6 +27,30 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+type broadcastMsg struct {
+	Message map[string]interface{}
+	RoomID  string
+	Client  *websocket.Conn
+}
+
+var broadcast = make(chan broadcastMsg)
+
+func broadcaster() {
+	for {
+		msg := <-broadcast
+
+		for _, client := range AllRoom.Map[msg.RoomID] {
+			if client.conn != msg.Client {
+				err := client.conn.WriteJSON(msg.Message)
+				if err != nil {
+					log.Fatal(err)
+					client.conn.Close()
+				}
+			}
+		}
+	}
 }
 
 func JoinRoomRequestHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,5 +68,19 @@ func JoinRoomRequestHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	AllRoom.InsertIntoRoom(roomID[0], false, ws)
+	go broadcaster()
 
+	for {
+		var msg broadcastMsg
+
+		err := ws.ReadJSON(&msg.Message)
+		if err != nil {
+			log.Fatal("Read Error: ", err)
+
+		}
+
+		msg.Client = ws
+		msg.RoomID = roomID[0]
+		broadcast <- msg
+	}
 }
